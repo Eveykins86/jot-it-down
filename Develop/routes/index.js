901ee58b2ccd80e49a85dbe9de5f5d907b128d54
Evelyn Jimenez <evey.plt@gmail.com>
2023-07-23 15:51:36 -0500

    added code to index.js, notes.js fixed file path in notes.html

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const notesRouter = require('./notes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use('/notes', notesRouter);
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT')
+});
 
 module.exports = app;
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,12 +1,36 @@
 const notesRouter = require('express').Router();
 const fs = require('fs');
+const path = require('path');
 
+const notesFilePath = path.join(__dirname, '../db/db.json');
+
+//Helper function to read notes from the JSON file
+function readNotesFromFile() {
+    const data = fs.readFileSync(notesFilePath, 'utf8');
+}
+
+//Helper function to write notes to the JSON file
+function writeNotesToFile(notes) {
+    fs.writeFileSync(notesFilePath, JSON.stringify(notes), 'utf8');
+}
+
+//Route to get all notes
 notesRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+//Route to save a new note
 notesRouter.post('/', (req, res) => {
-    
+    const newNote = req.body;
+    //Generate a unique ID for the new note
+    newNote.id = generateUniqueId();
+
+    const notes = readNotesFromFile();
+    notes.push(newNote);
+
+    writeNotesToFile(notes);
+
+    res.json(newNote);
 });
 
-module.exports = notesRouter
+module.exports = notesRouter;
