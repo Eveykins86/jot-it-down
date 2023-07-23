c541bf90e2c597f7eb0764e74c478901007f83e0
Evelyn Jimenez <evey.plt@gmail.com>
2023-07-23 16:44:52 -0500

    Moved files to correct locations

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,27 @@
+//Import required modules
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
 
+//Create Express application
 const app = express();
 const PORT = 3001;
 
+//Middleware setup
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use('/api', api);
 
+//Route for the root path '/'
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+//Sart the server
+const server = app.listen(PORT, () => {
     console.log('application succsefully listening to http://localhost:${PORT}')
 })
-.catch((err) => {
-    console.log(err);
-})
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
