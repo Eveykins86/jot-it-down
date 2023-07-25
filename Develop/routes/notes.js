811dd7050de2d8af53255088e29c7b5cffed4d7c
Evelyn Jimenez <evey.plt@gmail.com>
2023-07-24 22:44:39 -0500

    fixed code in notes.js to have new notes saved into db.json, added reload code to server.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -6,46 +6,41 @@ const { v4: uuidv4 } = require('uuid');
 const dbPath = path.join(__dirname, '../db/db.json');
 
 //retrieves all notes from db.json file
-notesRouter.get('/api/notes', (req, res) => {
+notesRouter.get('/notes', (req, res) => {
     let notes = JSON.parse(fs.readFileSync(dbPath))
     res.json(notes);
 })
 
 
 // Creates new note and adds it to db.json file
-notesRouter.post('api/notes', (req, res) => {
+notesRouter.post('/notes', (req, res) => {
     let notes = JSON.parse(fs.readFileSync(dbPath))
-    req.body.id = uuidv4;
-    notes.push
+    req.body.id = uuidv4();
+    notes.push(req.body);
+    fs.writeFileSync(dbPath, JSON.stringify(notes));
+    res.status(200);
 })
 
 //Route to delete note
-notesRouter.delete('/api/notes/:id', (req, res) => {
+notesRouter.delete('/notes/:id', (req, res) => {
     let requestedId = req.params.id;
-    fs.readFile(dbPath, 'utf8', (err, data) => {
-        if(err) {
-            console.error(err);
-        }
-        else {
-            const parsedNotes = JSON.parse(data);
-            for(let i=0; i<parsedNotes.length; i++) {
-                const currentNote = parsedNotes[i];
-                if(currentNote.id === requestedId) {
-                    parsedNotes.splice(i, 1);
-                }
-            }
-            fs.writeFile(dbPath, JSON.stringify(parsedNotes, null, 4), (err) => {
-                err ? console.error(err) : console.log('Successfully deleted note!')
-            }
-            );
-        }
-    });
-    //create success message
-    const response = {
-        status: 'Note successfully deleted!',
-        id: requestedId,
+    try {
+        const data = fs.readFileSync(dbPath, 'utf8');
+        const notes = JSON.parse(data);
+        const updatedNotes = notes.filter((note) => note.id !== requestedId);
+
+        fs.writeFileSync(dbPath, JSON.stringify(updatedNotes, null, 4));
+
+        // Create success message
+        const response = {
+            status: 'Note successfully deleted!',
+            id: requestedId,
+        };
+        res.json(response);
+    } catch (error) {
+        console.error('Error reading/parsing db.json:', error);
+        res.status(500).json({ error: 'Failed to read data from the database.' });
     }
-    res.json(response);
-})
+});
 
 module.exports = notesRouter;
